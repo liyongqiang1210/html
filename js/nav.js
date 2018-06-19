c6693e4f348986a7cf6085882b130301dded6ec5
liyongqiang <1240965061@qq.com>
2018-06-19 18:58:13 +0800

    初始化项目

--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,33 @@
 $(function(){
-    // nav-left收缩展开
+    // nav收缩展开
     $('.nav-left-item>a').on('click',function(){
         if (!$('.nav-left').hasClass('nav-left-mini')) {
             if ($(this).next().css('display') == "none") {
                 //展开未展开
-                $('.nav-left-item').children('ul').slideUp(300);
-                $(this).next('ul').slideDown(300);
+                $('.nav-left-item').children('ul').slideUp(200);
+                $(this).next('ul').slideDown(200);
                 $(this).parent('li').addClass('nav-left-show').siblings('li').removeClass('nav-left-show');
             }else{
                 //收缩已展开
-                $(this).next('ul').slideUp(300);
+                $(this).next('ul').slideUp(200);
                 $('.nav-left-item.nav-left-show').removeClass('nav-left-show');
             }
         }
     });
-    //nav-left-mini切换
-    $('#mini').on('click',function(){
+    //nav-mini切换
+    $('.nav-left-top').on('click',function(){
         if (!$('.nav-left').hasClass('nav-left-mini')) {
             $('.nav-left-item.nav-left-show').removeClass('nav-left-show');
             $('.nav-left-item').children('ul').removeAttr('style');
             $('.nav-left').addClass('nav-left-mini');
-            $('.content-main').css("left","60px");
-            $('.route-nav').css("left","60px");
+            $('.route_nav').css('margin-left','60px');
+            $('.nav-left-top').css({'width':'60px','padding-left':'20px','padding-right':'20px'});
+            $('.main').css('padding-left','60px');
         }else{
             $('.nav-left').removeClass('nav-left-mini');
-            $('.content-main').css("left","220px");
-            $('.route-nav').css("left","220px");
+            $('.route_nav').css('margin-left','220px');
+             $('.nav-left-top').css({'width':'220px','padding-left':'100px','padding-right':'100px'});
+              $('.main').css('padding-left','220px');
         }
     });
 });
