2be6caf79158a8d57a419f6c63ffc34cb1beed61
liyongqiang1210 <1240965061@qq.com>
2018-07-20 17:35:31 +0800

    修改移动窗口js

--- js/move_port.js
+++ js/move_port.js
@@ -7,11 +7,11 @@ function move_obj(obj,move_w,move_h,x,y,l,t,m){
   }
   move_w=(move_w==undefined||move_w=='')?$(window).width():move_w;//水平移动范围,默认浏览器可视区域宽度
   move_h=(move_h==undefined||move_h=='')?$(window).height():move_h;//垂直移动范围,默认浏览器可视区域高度
-  x=(x==undefined||x=='')?3:x;//一次水平移动距离,默认3px
-  y=(y==undefined||y=='')?3:y;//一次垂直移动距离,默认3px
+  x=(x==undefined||x=='')?1:x;//一次水平移动距离,默认3px
+  y=(y==undefined||y=='')?1:y;//一次垂直移动距离,默认3px
   l=(l==undefined||l=='')?0:l;//相对于body的起始水平位置,默认0
   t=(t==undefined||t=='')?0:t;//相对于body的起始垂直位置,默认0
-  m=(m==undefined||m=='')?80:m;//计时周期,单位毫秒,默认80ms
+  m=(m==undefined||m=='')?20:m;//计时周期,单位毫秒,默认80ms
 
   function moving(){
     x=(l>=move_w-$(obj).width()||l<0)?-x:x;
